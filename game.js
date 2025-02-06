8a76a2695fda5392887a2aee54256bbeb6f6503e
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-02-06 14:56:15 -0800

    Reformat

--- game.js
+++ game.js
@@ -1,17 +1,3 @@
-/*
-Guess My Number
-Make a web page that generates a secret random number and the user can guess the number until they get it right. For each guess, the game should display whether the answer is higher, lower, or correct.
-
-Optional Extensions:
-
-(DONE)Track how many wrong guesses the user has made so far and display that count
-(DONE)Track what numbers the user has already guessed and display those
-
-Let the user choose the range of the secret number
-
-
-*/
-
 const secretNum = document.getElementById("secretNum");
 const userGuess = document.getElementById("userGuess");
 const guesses = document.getElementById("guesses");
@@ -20,32 +6,20 @@ const numMatch = document.getElementById("numMatch");
 const submitBtn = document.getElementById("submitBtn");
 const guessCount = document.getElementById("guessCount");
 const counter = document.getElementById("counter");
-const myRange = document.getElementById("myRange");
-const output = document.getElementById("output");
 
-const max = 100;
-const min = 1;
-
-output.innerHTML = '5';
-
-console.log(output.textContent = myRange.value);
-
-myRange.oninput = function(){
-    output.innerHTML = this.value;
-}
-
-
-
+let max = 100;
+let min = 1;
 
 let Num = Math.floor(Math.random() * (max-min) + min);
-secretNum.textContent = Num //we are changing the text content of secret Num Div to hold our num
-//the display will be hidden
-
+secretNum.textContent = Num 
 numMatch.style.display = "none";
-let wrongGuess = [];
-//secretNum.style.display = "none" //the default for this is set to none
+secretNum.style.display = "none"
 
 let count = 0;
+let wrongGuess = [];
+
+console.log(Num);
+
 
 submitBtn.addEventListener("click", function (){
     count++;
@@ -76,8 +50,30 @@ submitBtn.addEventListener("click", function (){
 
 
 
+//The section below holds the code for my range slider, I will implement this 
+//in a future iteration, the secret number is generating before the range is set. 
 
-//to store the count, can I collect the number of times the button was clicked?
-//next steps, I need to connect the min and max range choosen to connect to my functions
+/*const myRange = document.getElementById("myRange");
+const min = myRange.min;
+let max = parseInt(myRange.max);
+const defaultValue = myRange.value;
+const output = document.getElementById("output");
+myRange.oninput = function(){
+   max = parseInt(this.value);
+   output.innerHTML = max
+}
+*/
+
+/*
+Guess My Number
+Make a web page that generates a secret random number and the user can guess the number until they get it right. For each guess, the game should display whether the answer is higher, lower, or correct.
+
+Optional Extensions:
+
+(DONE)Track how many wrong guesses the user has made so far and display that count
+(DONE)Track what numbers the user has already guessed and display those
+
+Let the user choose the range of the secret number
 
 
+*/
